4d788d01124828562ef81a308706de36d0c82d92
Venkateswarlu <ashavenkateswarlu@gmail.com>
2025-10-19 05:53:33 +0000

    code uploaded

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const NotFound = () => (
+  <>
+    <div className="not-found-bg">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+        alt="not found"
+        className="not-found-img"
+      />
+      <h1 className="not-head">Page Not Found</h1>
+      <p className="not-para">
+        We are sorry, the page you requested could not be found
+      </p>
+    </div>
+  </>
+)
+export default NotFound
